8745178a3ecdc0af22cdc58e363e88a38b7a1244
phatlt-jienielab <phatlt@jienielab.asia>
2022-10-12 22:06:31 +0700

    add / update api for admin

--- src/actions/master/post-action.js
+++ src/actions/master/post-action.js
@@ -1,119 +1,161 @@
-const pool = require('../../../configs/psql-connect');
-const postRepo = require('../../respository/master/post-repo');
-const commonFunction = require('../../actions/common-action');
-const linkDownloadFunction = require('../../actions/master/link-download-action');
-const drive = require('../../actions/drive');
+const pool = require("../../../configs/psql-connect");
+const postRepo = require("../../respository/master/post-repo");
+const commonFunction = require("../../actions/common-action");
+const linkDownloadFunction = require("../../actions/master/link-download-action");
+const drive = require("../../actions/drive");
 
 save = async function (req, res) {
-    try {
-        const request = req.body;
-        request.code = commonFunction.toKebab(request.code);
+  try {
+    const request = req.body;
+    request.code = commonFunction.toKebab(request.code);
 
-        let post = await pool.query(postRepo.GET_BY_ID, [request.id]);
+    let post = await pool.query(postRepo.GET_BY_ID_AND_CODE, [
+      request.id,
+      request.code,
+    ]);
 
-        // Save post and return id to save links and images
-        let postId;
-        let sqlRes
-        if (post.rows.length) {
-            sqlRes = await pool.query(postRepo.UPDATE, [request.code, request.name, request.categoryCode, request.title, request.description, new Date(), request.content, request.id]);
-        } else {
-            sqlRes = await pool.query(postRepo.INSERT, [request.code, request.name, request.categoryCode, request.title, request.description, request.content, new Date(), new Date()]);
-        }
+    // Save post and return id to save links and images
+    let postId = request.id;
+    if (post.rows.length) {
+      // Check is different before update
+      const oldPost = post.rows[0];
+      const newPost = request;
+      let isDifferent =
+        oldPost.categoryCode !== oldPost.categoryCode ||
+        oldPost.title !== newPost.title ||
+        oldPost.description !== newPost.description ||
+        oldPost.name !== newPost.name ||
+        oldPost.content !== newPost.content;
+
+      if (isDifferent) {
+        // Insert backup record
+        await pool.query(postRepo.INSERT_BACKUP, [request.id]);
+
+        // Update current record to new version
+        const sqlRes = await pool.query(postRepo.UPDATE, [
+          request.code,
+          request.name,
+          request.categoryCode,
+          request.title,
+          request.description,
+          new Date(),
+          request.content,
+          request.id,
+        ]);
+
         postId = sqlRes.rows[0].id;
+      }
+    } else {
+      const sqlRes = await pool.query(postRepo.INSERT, [
+        request.code,
+        request.name,
+        request.categoryCode,
+        request.title,
+        request.description,
+        request.content,
+        new Date(),
+        new Date(),
+      ]);
 
-        // Save link by post id
-        linkDownloadFunction.saveLinkDownloadByPostId(request.linkDownload, postId);
-
-        // Save banner image
-        saveBannerImage(postId, request.imgUrl, request.code);
-
-        res.status(200).send();
-    } catch (err) {
-        console.error("Save post failed:", err);
-        res.status(400).send({ mes: err });
+      postId = sqlRes.rows[0].id;
     }
-}
+
+    // Save link by post id
+    linkDownloadFunction.saveLinkDownloadByPostId(request.linkDownload, postId);
+
+    // Save banner image
+    saveBannerImage(postId, request.imgUrl, request.code);
+
+    res.status(200).send();
+  } catch (err) {
+    console.error("Save post failed:", err);
+    res.status(400).send({ mes: err });
+  }
+};
 
 loadList = async function (req, res) {
-    try {
-        const categoryCode = req.body.categoryCode;
+  try {
+    const categoryCode = req.body.categoryCode;
 
-        const listSqlResult = await pool.query(postRepo.GET_LIST, [categoryCode]);
-        const postList = listSqlResult.rows;
+    const listSqlResult = await pool.query(postRepo.GET_LIST, [categoryCode]);
+    const postList = listSqlResult.rows;
 
-        res.status(200).send(postList);
-    } catch (err) {
-        console.error("Get post list failed:", err);
-        res.status(400).send({ mes: err });
-    }
-}
+    res.status(200).send(postList);
+  } catch (err) {
+    console.error("Get post list failed:", err);
+    res.status(400).send({ mes: err });
+  }
+};
 
 loadDetail = async function (req, res) {
-    try {
-        const postCode = req.body.postCode;
+  try {
+    const postCode = req.body.postCode;
 
-        const postSqlResult = await pool.query(postRepo.GET_BY_CODE, [postCode]);
-        const rawPost = postSqlResult.rows[0];
-        const post = {
-            id: rawPost.id,
-            code: rawPost.code,
-            name: rawPost.name,
-            categoryCode: rawPost.category_code,
-            title: rawPost.title,
-            description: rawPost.description,
-            content: rawPost.content,
-            createdDate: rawPost.created_date,
-            updatedDate: rawPost.updated_date,
-            downloadNum: rawPost.download_num,
-            readNum: rawPost.read_num,
-        }
+    const postSqlResult = await pool.query(postRepo.GET_BY_CODE, [postCode]);
+    const rawPost = postSqlResult.rows[0];
+    const post = {
+      id: rawPost.id,
+      code: rawPost.code,
+      name: rawPost.name,
+      categoryCode: rawPost.category_code,
+      title: rawPost.title,
+      description: rawPost.description,
+      content: rawPost.content,
+      createdDate: rawPost.created_date,
+      updatedDate: rawPost.updated_date,
+      downloadNum: rawPost.download_num,
+      readNum: rawPost.read_num,
+    };
 
-        let links = await linkDownloadFunction.getLinkDownloadByPostId(post.id);
+    let links = await linkDownloadFunction.getLinkDownloadByPostId(post.id);
 
-        res.status(200).send({
-            post: post,
-            links: links
-        });
-    } catch (err) {
-        console.error("Save post failed:", err);
-        res.status(400).send({ mes: err });
-    }
-}
+    res.status(200).send({
+      post: post,
+      links: links,
+    });
+  } catch (err) {
+    console.error("Save post failed:", err);
+    res.status(400).send({ mes: err });
+  }
+};
 
 uploadImage = async function (req, res) {
-    try {
-        drive.uploadFile(req.file, res, saveImage);
-    } catch (err) {
-        console.error("upload image failed:", err);
-        res.status(400).send({ mes: err });
-    }
-}
+  try {
+    drive.uploadFile(req.file, res, saveImage);
+  } catch (err) {
+    console.error("upload image failed:", err);
+    res.status(400).send({ mes: err });
+  }
+};
 
 saveImage = function (res, file) {
-    let imageUrl = "https://drive.google.com/uc?id=" + file.imageId;
+  let imageUrl = "https://drive.google.com/uc?id=" + file.imageId;
 
-    let result = {
-        fileName: file.fileName,
-        uploaded: 1,
-        url: imageUrl
-    }
-    res.status(200).send(result);
-}
+  let result = {
+    fileName: file.fileName,
+    uploaded: 1,
+    url: imageUrl,
+  };
+  res.status(200).send(result);
+};
 
 saveBannerImage = async function (postId, imgUrl, alt) {
-    // Get all link existed before register new
-    await pool.query(postRepo.DELETE_BANNER_IMAGE, [postId]);
+  // Get all link existed before register new
+  await pool.query(postRepo.DELETE_BANNER_IMAGE, [postId]);
 
-    try {
-        await pool.query(postRepo.SAVE_BANNER_IMAGE, [postId, imgUrl, alt]);
-    } catch (err) {
-        console.error("save banner image failed:", err);
-    }
-}
+  // Replace google photo image by thumbnail
+  imgUrl = imgUrl.replace("/uc?", "/thumbnail?");
+
+  try {
+    await pool.query(postRepo.SAVE_BANNER_IMAGE, [postId, imgUrl, alt]);
+  } catch (err) {
+    console.error("save banner image failed:", err);
+  }
+};
 
 module.exports = {
-    save,
-    uploadImage,
-    loadList,
-    loadDetail
-}
+  save,
+  uploadImage,
+  loadList,
+  loadDetail,
+};
--- src/respository/master/post-repo.js
+++ src/respository/master/post-repo.js
@@ -11,15 +11,24 @@ module.exports = {
     FROM post
     INNER JOIN category ON post.category_code = category.code
     WHERE $1::text IS NULL OR post.category_code = $1`,
-  GET_BY_ID: "SELECT post.* FROM post WHERE post.id = $1",
+  GET_BY_ID_AND_CODE:
+    "SELECT post.* FROM post WHERE post.id = $1 AND post.code = $2",
   GET_BY_CODE: "SELECT post.* FROM post WHERE post.code = $1",
-  INSERT: `INSERT INTO post (code, name, category_code, title, description, content, created_date, updated_date)
+  INSERT: `INSERT INTO post (code, name, category_code, title, description,
+      content, created_date, updated_date)
     VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
     RETURNING id`,
   UPDATE: `UPDATE post
-    SET code = $1, name = $2, category_code = $3, title = $4, description = $5, updated_date = $6, content = $7
+    SET code = $1, name = $2, category_code = $3, title = $4, description = $5,
+      updated_date = $6, content = $7, version = version + 1
     WHERE id = $8
     RETURNING id`,
+  INSERT_BACKUP: `INSERT INTO post_backup (code, name, category_code, title,
+      description, content, created_date, updated_date, version)
+    SELECT code, name, category_code, title, description, content, created_date,
+      updated_date, version
+    FROM post
+    WHERE post.id = $1`,
   DELETE_BANNER_IMAGE: "DELETE FROM post_image WHERE post_image.post_id = $1",
   SAVE_BANNER_IMAGE: `INSERT INTO post_image (post_id, url, name)
     VALUES ($1, $2, $3)`,
